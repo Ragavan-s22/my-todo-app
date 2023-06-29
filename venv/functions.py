2e36f0c6fed9871a8b7e0e83a40ea74e2a3d86bf
ragav <ragavsss2004@gmail.com>
2023-06-30 05:24:55 +0530

    Initial Commit 3

--- venv/functions.py
+++ venv/functions.py
@@ -1,11 +1,11 @@
-def get_todos(filepath="todos.txt"):
+def get_todos(filepath="venv/todos.txt"):
     """ Read a text file and return the list of todos """
     with open(filepath, 'r') as file_local:
         todos_local = file_local.readlines()
     return todos_local
  
 
-def write_todos(todos_arg , filepath="todos.txt"):
-    """ '"""
+def write_todos(todos_arg , filepath="venv/todos.txt"):
+    """ Write a text file """
     with open(filepath, 'w') as file_local:
         file_local.writelines(todos_arg)
